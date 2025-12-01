1e0746fa68e69551d7ce4ab2d15ac9f8c8df9b7e
Skander <ibenamor@ats-digital.com>
2025-12-01 07:24:48 +0100

    day1:star 1

--- day 1/day1.ts
+++ day 1/day1.ts
@@ -1,4 +1,62 @@
-const path = "/path/to/file.txt";
-const file = Bun.file(path);
+import { readFileSync } from "node:fs";
 
-const text = await file.text();
+const dialStart = 50;
+let safeSeq = "";
+
+try {
+	const filePath = "day 1/safe_sequence.txt";
+	const fileContent: string = readFileSync(filePath, "utf8");
+	safeSeq = fileContent;
+} catch (error) {
+	console.error("Error reading file:", error);
+}
+function extractMove(input: string): {
+	move: string | null;
+	distance: number;
+} {
+	const regex = /^([a-zA-Z]+)(\d+)/;
+
+	const matchResult = input.match(regex);
+
+	if (matchResult) {
+		const move = matchResult[1];
+		const distance = parseInt(matchResult[2], 10);
+
+		return { move, distance };
+	} else {
+		return { move: null, distance: 0 };
+	}
+}
+function incrementNumber(start: number, move: string | null, distance: number) {
+	if (distance === 0) {
+		return start;
+	}
+	if (move === "L") {
+		if (start === 0) {
+			return incrementNumber(99, move, distance - 1);
+		}
+		return incrementNumber(start - 1, move, distance - 1);
+	}
+	if (move === "R") {
+		if (start === 99) {
+			return incrementNumber(0, move, distance - 1);
+		}
+		return incrementNumber(start + 1, move, distance - 1);
+	}
+	return 0;
+}
+
+(function coutZeros() {
+	const moves = safeSeq.split("\n").reduce(
+		([lastNumber, numberOfZeros], line) => {
+			const { move, distance } = extractMove(line);
+			const newNumber = incrementNumber(lastNumber, move, distance);
+			if (newNumber === 0) {
+				return [newNumber, numberOfZeros + 1];
+			}
+			return [newNumber, numberOfZeros];
+		},
+		[dialStart, 0],
+	);
+	console.log("result: ", moves);
+})();
