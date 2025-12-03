f7fc36607d25517c2b036667c7bc2db700af3538
Skander <ibenamor@ats-digital.com>
2025-12-03 07:51:51 +0100

    day 3: part 2 hardcoded...

--- day 3/day3.ts
+++ day 3/day3.ts
@@ -20,17 +20,93 @@ function biggestDigitIndex(arr: any[]): number {
 (function largestJoltagesSum() {
 	const result = banks.split("\n").reduce((sum, bank) => {
 		const batteries = bank.split("");
+
 		const firstDigitIdx = biggestDigitIndex(
-			batteries.slice(0, batteries.length - 1),
+			batteries.slice(0, batteries.length - 11),
 		);
-		const secondDigitIdx = biggestDigitIndex(
-			batteries.slice(firstDigitIdx + 1),
-		);
+		const secondDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(firstDigitIdx + 1, batteries.length - 10),
+			) +
+			firstDigitIdx +
+			1;
+		const thirdDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(secondDigitIdx + 1, batteries.length - 9),
+			) +
+			secondDigitIdx +
+			1;
+		const fourthDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(thirdDigitIdx + 1, batteries.length - 8),
+			) +
+			thirdDigitIdx +
+			1;
+		const fithDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(fourthDigitIdx + 1, batteries.length - 7),
+			) +
+			fourthDigitIdx +
+			1;
+		const sixthDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(fithDigitIdx + 1, batteries.length - 6),
+			) +
+			fithDigitIdx +
+			1;
+		const seventhDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(sixthDigitIdx + 1, batteries.length - 5),
+			) +
+			sixthDigitIdx +
+			1;
+		const eightDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(seventhDigitIdx + 1, batteries.length - 4),
+			) +
+			seventhDigitIdx +
+			1;
+		const ninethDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(eightDigitIdx + 1, batteries.length - 3),
+			) +
+			eightDigitIdx +
+			1;
+		const tenthDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(ninethDigitIdx + 1, batteries.length - 2),
+			) +
+			ninethDigitIdx +
+			1;
+		const eleventhDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(tenthDigitIdx + 1, batteries.length - 1),
+			) +
+			tenthDigitIdx +
+			1;
+		const twelvethDigitIdx =
+			biggestDigitIndex(
+				batteries.slice(eleventhDigitIdx + 1, batteries.length),
+			) +
+			eleventhDigitIdx +
+			1;
 
-		const firstDigit = batteries[firstDigitIdx];
-		const secondDigit = batteries[secondDigitIdx + 1 + firstDigitIdx];
+		const combination = [
+			batteries[firstDigitIdx],
+			batteries[secondDigitIdx],
+			batteries[thirdDigitIdx],
+			batteries[fourthDigitIdx],
+			batteries[fithDigitIdx],
+			batteries[sixthDigitIdx],
+			batteries[seventhDigitIdx],
+			batteries[eightDigitIdx],
+			batteries[ninethDigitIdx],
+			batteries[tenthDigitIdx],
+			batteries[eleventhDigitIdx],
+			batteries[twelvethDigitIdx],
+		];
 
-		return sum + parseInt(`${firstDigit}${secondDigit}`, 10);
+		return sum + parseInt(combination.join(""), 10);
 	}, 0);
 	console.log("result: ", result);
 })();
