2bc4f27ad29e6325b06d5b669c31b1b8940040a7
Skander <ibenamor@ats-digital.com>
2025-12-07 07:48:30 +0100

    day 7: part 2

--- day 7/day7.ts
+++ day 7/day7.ts
@@ -1,19 +1,17 @@
 import { readFileSync } from "node:fs";
 
-let mathProblems = "";
+let manifolds = "";
 
 try {
 	const filePath = "day 7/manifolddiagram.txt";
 	const fileContent: string = readFileSync(filePath, "utf8");
-	mathProblems = fileContent;
+	manifolds = fileContent;
 } catch (error) {
 	console.error("Error reading file:", error);
 }
 
-const operatorRegex = /[+*]/g;
-
 (function countSplits() {
-	const lines = mathProblems.split("\n");
+	const lines = manifolds.split("\n");
 	let beamIndexes = [lines[0].indexOf("S")];
 	let numberOfSplits = 0;
 	for (let i = 1; i < lines.length; i++) {
@@ -31,4 +29,41 @@ const operatorRegex = /[+*]/g;
 	}
 
 	console.log(`a tachyon beam is split a total of ${numberOfSplits} times.`);
+})();
+
+(function countTimelines() {
+	const lines = manifolds.split("\n");
+	let beamCounts = new Map([[lines[0].indexOf("S"), 1]]);
+	for (let i = 1; i < lines.length; i++) {
+		// const newBeamIndex: Array<number> = [];
+		const nextBeamCounts: Map<number, number> = new Map();
+		for (const [beamIndex, beamCount] of beamCounts) {
+			if (lines[i][beamIndex] === "^") {
+				const indexLeft = beamIndex - 1;
+				nextBeamCounts.set(
+					indexLeft,
+					(nextBeamCounts.get(indexLeft) || 0) + beamCount,
+				);
+				const indexRight = beamIndex + 1;
+				nextBeamCounts.set(
+					indexRight,
+					(nextBeamCounts.get(indexRight) || 0) + beamCount,
+				);
+			} else {
+				nextBeamCounts.set(
+					beamIndex,
+					(nextBeamCounts.get(beamIndex) || 0) + beamCount,
+				);
+			}
+		}
+		beamCounts = nextBeamCounts;
+	}
+
+	let numberOfTimeLines = 0;
+	for (const count of beamCounts.values()) {
+		numberOfTimeLines += count;
+	}
+	console.log(
+		`the particle ends up on ${numberOfTimeLines} different timelines.`,
+	);
 })();
